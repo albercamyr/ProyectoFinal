0d40c1036d38cca57b6d488f2fa523ff3f4c4c2a
Myriam Alberca <albercamyr@gmail.com>
2017-03-06 16:10:34 -0500

    segundo commit

---
+++ assets/JS/main.js
@@ -0,0 +1,8 @@
+var mostarOcultar = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
